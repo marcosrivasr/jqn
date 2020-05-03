351e17d4610daaf4d63ca950e3821e0cb5286534
Marcos Rivas R <marcosrivasr@gmail.com>
2020-05-02 19:02:49 -0500

    new functions

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Native jQuery
 
-Library to implement jQuery most popular functions with Vanilla Javascript
+Library to implement jQuery most popular functions with Vanilla Javascript.
+
+## Usage
+
+To begin using the library it's important to create a new ```El``` object
+
+```javascript
+let button = new El('#btn');
+```
--- main.js
+++ main.js
@@ -2,12 +2,16 @@
 class El{
 
     constructor(selector){
+        //console.log(selector);
         if(typeof selector === 'object'){
             this.selector = null;
             this.element = selector;
+            this.height = null;
         }else if(typeof selector === 'string'){
             this.selector = selector;
             this.element = document.querySelector(selector);
+            this.height = this.element.offsetHeight;
+            this.element.style.height = this.height + 'px';
         }else if(!selector){
             throw new Error('no selector specified at the constructor');
         }
@@ -74,8 +78,38 @@ class El{
         return this;
     }
 
-    animate(properties){
+    slideUp(time = 3000){
+    
+        if(this.height === null) throw new Error('no height defined');
 
+        this.element.style['overflow-y'] = 'hidden';
+        this.height = this.element.offsetHeight;
+        
+        this.element.style.transitionProperty = 'height !important';
+        this.element.style.height = '0px';
+        this.element.style.transitionDuration = (time/1000) + 's';
+    }
+
+    slideDown(time = 3000){
+        if(this.height === null) throw new Error('no height defined');
+
+        this.element.style['overflow-y'] = 'hidden';
+        
+        this.element.style.transitionProperty = 'height !important';
+        this.element.style.height = this.height + 'px';
+        this.element.style.transitionDuration = (time/1000) + 's';
+    }
+
+    animate(properties, time = 3000){
+        const keys = Object.keys(properties);
+        const values = Object.values(properties);
+
+        for(let i=0; i<keys.length; i++){
+
+            this.element.style[keys[i]] = values[i];
+            this.element.style.transitionProperty = keys[i] + ' !important';
+            this.element.style.transitionDuration = (time/1000) + 's';
+        }
     }
 
 
@@ -87,8 +121,7 @@ function get(url, options){
 
 async function getJSON(url, options){
     try{
-        return await fetch(url, options)
-                            .then(res => res.json());
+        return await fetch(url, options).then(res => res.json());
     }catch(error){
         return error;
     }
@@ -96,7 +129,9 @@ async function getJSON(url, options){
 
 async function getText(url, options){
     try{
-        return await fetch(url, options).then(res => res.text());
+        const response = fetch(url, options);
+        const text = await response.then(data => data.text());
+        //return text
     }catch(error){
         return error;
     }
